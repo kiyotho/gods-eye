80edc3865bfedc2d11b25f7fb3f88ec682fd3dac
Kiyotho <somayojha2@gmail.com>
2026-05-30 21:09:43 +0530

    changed the globe and font

--- public/index.js
+++ public/index.js
@@ -3,15 +3,27 @@ import * as THREE from 'three'
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000 )
 
-const renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer({antialias : true})
 renderer.setSize( window.innerWidth, window.innerHeight)
 document.body.appendChild( renderer.domElement )
 
 
+// const imageLoader = new THREE.ImageLoader() 
+// imageLoader.load('../src/earthlights1k.jpg', (image) => {
+
+// })
+
+
 const geometry = new THREE.SphereGeometry(5, 32, 32)
 const loader = new THREE.TextureLoader()
-const texture = loader.load('https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/WorldMap-A_non-Frame.png/1280px-WorldMap-A_non-Frame.png')
-const material = new THREE.MeshPhongMaterial({ map: texture })
+const texture = loader.load('./earthlights1k.jpg')
+const material = new THREE.MeshPhongMaterial({ 
+    map: texture,
+    transparent: true, 
+    opacity: 0.8,
+    side: THREE.FrontSide
+
+})
 
 
 const sphere = new THREE.Mesh( geometry, material )
@@ -19,9 +31,9 @@ const ambientLight = new THREE.AmbientLight(0xffffff, 1)
 scene.add(ambientLight)
 scene.add( sphere )
 
-camera.position.z = 13
+camera.position.z = 9
 
-const glowGeometry = new THREE.SphereGeometry(5.3, 32, 32)
+const glowGeometry = new THREE.SphereGeometry(5.15, 32, 32)
 const glowMaterial = new THREE.MeshPhongMaterial({
 
     color: 0x0088ff,
@@ -38,7 +50,7 @@ scene.add(glow)
 function animate(time) {
     renderer.render(scene, camera)
 
-    sphere.rotation.y = time / 1000
+    // sphere.rotation.y = time / 1000
 }
 
 renderer.setAnimationLoop( animate )
